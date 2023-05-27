94e99591c27b8db10b848f1cc839e5b65d495547
JakubKotulski <jakub.kotulski@interia.com>
2023-05-27 12:15:43 +0200

    have to push

--- src/components/pages/LoginPage/Login.js
+++ src/components/pages/LoginPage/Login.js
@@ -1,12 +1,12 @@
 import { useForm } from "react-hook-form";
 import { Form, FloatingLabel, Button } from "react-bootstrap";
 import "./Login.css";
+import { useState } from "react";
 
 const Login = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
 
@@ -20,15 +20,21 @@ const Login = () => {
     },
   };
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const getEmail = (e) => setEmail(e.target.value);
+  const getPassword = (e) => setPassword(e.target.value);
+
   return (
     <div className="custom-container">
       <Form className="custom-form" autoComplete="off" onSubmit={handleSubmit((data) => {})}>
         <h3 className="form-header">Sign in</h3>
-        <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
+        <FloatingLabel onChange={getEmail} controlId="floatingInput" label="Email address" className="mb-3">
           <Form.Control type="email" placeholder="email" {...register("email", loginOptions.email)} />
           <small className="text-error">{errors.email && errors.email.message}</small>
         </FloatingLabel>
-        <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3">
+        <FloatingLabel onChange={getPassword} controlId="floatingPassword" label="Password" className="mb-3">
           <Form.Control type="password" placeholder="Password" {...register("password", loginOptions.password)} />
           <small className="text-error">{errors.password && errors.password.message}</small>
         </FloatingLabel>
--- src/components/pages/RegisterPage/Register.js
+++ src/components/pages/RegisterPage/Register.js
@@ -1,12 +1,15 @@
 import { useForm } from "react-hook-form";
 import { Form, FloatingLabel, Button } from "react-bootstrap";
 import "./Register.css";
+import { useState } from "react";
+import axios from "axios";
+import { backendUrl } from "../../../config";
+import ModalAlert from "../../ModalAlert/ModalAlert";
 
 const Register = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
 
@@ -24,43 +27,82 @@ const Register = () => {
     surname: {
       required: "Surname is required",
     },
-    birthday: {
-      required: "This field is required",
-    },
+  };
+
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [show, setShow] = useState(false);
+
+  const getName = (e) => setName(e.target.value);
+  const getSurname = (e) => setSurname(e.target.value);
+  const getEmail = (e) => setEmail(e.target.value);
+  const getPassword = (e) => setPassword(e.target.value);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const clearMessage = () => setMessage("");
+
+  const signUp = () => {
+    axios({
+      method: `POST`,
+      url: `${backendUrl}/user`,
+      data: {
+        name: name,
+        surname: surname,
+        email: email,
+        password: password,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("token", res.data.token);
+      })
+      .catch((err) => {
+        setMessage(err.response.data);
+        handleShow(true);
+        setName("");
+      });
   };
 
   return (
     <div className="custom-container">
-      <Form className="custom-form" autoComplete="off" onSubmit={handleSubmit((data) => {})}>
+      <Form
+        className="custom-form"
+        autoComplete="off"
+        onSubmit={(e) => {
+          e.preventDefault();
+          signUp();
+        }}
+      >
         <h3 className="form-header">Sign up</h3>
         <div className="divided-bar">
-          <FloatingLabel controlId="floatingInput" label="Name" className="mb-3">
+          <FloatingLabel onChange={getName} controlId="floatingName" label="Name" className="mb-3">
             <Form.Control type="texr" placeholder="name" {...register("name", loginOptions.name)} />
             <small className="text-error">{errors.name && errors.name.message}</small>
           </FloatingLabel>
-          <FloatingLabel controlId="floatingInput" label="Surname" className="mb-3">
+          <FloatingLabel onChange={getSurname} controlId="floatingSurname" label="Surname" className="mb-3">
             <Form.Control type="text" placeholder="surname" {...register("surname", loginOptions.surname)} />
             <small className="text-error">{errors.surname && errors.surname.message}</small>
           </FloatingLabel>
         </div>
-        <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
+        <FloatingLabel onChange={getEmail} controlId="floatingEmail" label="Email address" className="mb-3">
           <Form.Control type="email" placeholder="email" {...register("email", loginOptions.email)} />
           <small className="text-error">{errors.email && errors.email.message}</small>
         </FloatingLabel>
-        <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3">
+        <FloatingLabel onChange={getPassword} controlId="floatingPassword" label="Password" className="mb-3">
           <Form.Control type="password" placeholder="Password" {...register("password", loginOptions.password)} />
           <small className="text-error">{errors.password && errors.password.message}</small>
         </FloatingLabel>
-        <FloatingLabel controlId="floatingPassword" label="Birthday" className="mb-3">
-          <Form.Control type="date" placeholder="Birthday" {...register("birthday", loginOptions.birthday)} />
-          <small className="text-error">{errors.birthday && errors.birthday.message}</small>
-        </FloatingLabel>
         <Form.Group className="submit-center submit-group" controlId="formGroupSubmit">
           <Button type="submit" className="custom-button neon-border">
-            <span className="neon-button-text">Sign in</span>
+            <span className="neon-button-text">Sign up</span>
           </Button>
         </Form.Group>
       </Form>
+      <ModalAlert handleClose={handleClose} show={show} message={message} />
     </div>
   );
 };
